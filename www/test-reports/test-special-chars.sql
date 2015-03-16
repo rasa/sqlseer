f8ed1eb29d3780639e2d1df11c33a6ffd573ab3b
Ross Smith II <ross@smithii.com>
2015-03-16 11:50:54 -0400

    Add test-special-chars.sql

---
+++ www/test-reports/test-special-chars.sql
@@ -0,0 +1,72 @@
+/* Copyright (c) 2010-2015 Ross Smith II. MIT Licensed. */
+
+CREATE DATABASE IF NOT EXISTS temp; /*autorun=true*/
+
+DROP TABLE IF EXISTS temp.test_report_special_chars;
+
+CREATE TABLE temp.test_report_special_chars
+(
+  special_chars VARCHAR(255)
+);
+
+SET @chars = '*?><"\'\\%_=,~';
+
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+INSERT INTO temp.test_report_special_chars SET special_chars = MID(@chars, FLOOR(1+RAND()*LENGTH(@chars)), 1);
+
+SELECT /*autorun=true*/
+	*
+FROM
+  temp.test_report_special_chars t;
+
